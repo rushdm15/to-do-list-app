a42505e267d3078714c6b13ec06dc9e620b20249
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-10-10 21:04:32 +0200

    addEventListener => on Jquery

--- scripts.js
+++ scripts.js
@@ -21,14 +21,14 @@ function newItem(){
  		li.classList.toggle("strike");
  	}
 
- 	li.addEventListener("dblclick",crossOut);
+ 	li.on("dblclick",crossOut);
 
  //3(i). Adding the delete button "X":
    let crossOutButton = document.createElement("crossOutButton");
  	crossOutButton.appendChild(document.createTextNode("X"));
  	li.appendChild(crossOutButton);
 
- 	crossOutButton.addEventListener("click", deleteListItem);
+ 	crossOutButton.on("click", deleteListItem);
  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
    function deleteListItem(){
  		li.classList.add("delete")

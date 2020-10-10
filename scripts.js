ea4a8a6ce83d5a712723c8c7743f08fe7e729df1
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-10-10 21:56:00 +0200

    comments

--- scripts.js
+++ scripts.js
@@ -5,9 +5,9 @@ function newItem(){
 //1. Adding a new item to the list of items:
    let li = document.createElement("li");
    // let inputValue = document.getElementById("input").value; javascript
-   let inputValue = $('#input').val();
+   let inputValue = $('#input').val();  //jquery
    // let text = document.createTextNode(inputValue); javascript
-   li.appendChild(inputValue);
+   li.appendChild(inputValue);  //jquery
 
    if (inputValue === '') {
      alert("You must write something!");
@@ -27,6 +27,8 @@ function newItem(){
  //3(i). Adding the delete button "X":
    let crossOutButton = $("crossOutButton");
  	crossOutButton.appendChild(document.createTextNode("X"));
+   let crossOutButton = $("crossOutButton");  //jquery
+ 	deleteButton.append(document.createTextNode("X"));  //jquery
  	li.appendChild(crossOutButton);
 
  	crossOutButton.on("click", deleteListItem);
